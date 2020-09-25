076760831249510880071fde92c347ec06eca324
RuslanPark <0905.ruslan.pak.2001@mail.ru>
2020-09-25 22:11:38 +0600

    First commit

---
+++ WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.weatherapp
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Layout
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
