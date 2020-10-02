31222fce4875c5356d296638ceaa5826105c9721
RuslanPark <0905.ruslan.pak.2001@mail.ru>
2020-10-02 16:06:20 +0600

    Bug fixed

--- MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -1,13 +1,10 @@
 package com.example.myfirstapp
 
 import android.content.Intent
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
 import android.view.View
-import android.widget.ImageView
-import android.widget.LinearLayout
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
