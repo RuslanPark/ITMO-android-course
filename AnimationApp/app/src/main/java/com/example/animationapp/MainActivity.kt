6467b99e086c7aec2d02ec26b2d46c8f4e3ac69d
RuslanPark <0905.ruslan.pak.2001@mail.ru >
2021-02-09 22:40:45 +0600

    Migrate to ViewBinding

--- AnimationApp/app/src/main/java/com/example/animationapp/MainActivity.kt
+++ AnimationApp/app/src/main/java/com/example/animationapp/MainActivity.kt
@@ -2,24 +2,24 @@ package com.example.animationapp
 
 import android.animation.AnimatorInflater
 import android.animation.AnimatorSet
-import android.animation.ValueAnimator
 import android.os.Bundle
-import android.view.animation.LinearInterpolator
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
-
+import com.example.animationapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 
     override fun onResume() {
         super.onResume()
-
-        val animatorSet = AnimatorInflater.loadAnimator(textView.context, R.animator.text_view_launch) as AnimatorSet
-        animatorSet.setTarget(textView)
+        val animatorSet = AnimatorInflater.loadAnimator(binding.textView.context, R.animator.text_view_launch) as AnimatorSet
+        animatorSet.setTarget(binding.textView)
         animatorSet.start()
     }
 }
