6cac8ad5e7f34b263c37be64ff922c58832dd5f4
RuslanPark <0905.ruslan.pak.2001@mail.ru >
2021-03-16 16:01:49 +0600

    Update readme.md

--- WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -3,19 +3,20 @@ package com.example.weatherapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.view.get
 import com.example.weatherapp.databinding.ActivityMainBinding
 import com.example.weatherapp.databinding.WeatherTitleBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: WeatherTitleBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = WeatherTitleBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.switchTheme.setOnCheckedChangeListener { _, isChecked ->
+        WeatherTitleBinding.bind(binding.root).switchTheme.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
                 delegate.localNightMode = AppCompatDelegate.MODE_NIGHT_YES
             } else {
